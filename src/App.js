21f972c5e29780c4e5e1d9970b160dc6dbcd36bd
Kehinde Adeoso <kennolite@gmail.com>
2024-10-05 01:02:43 -0400

    Implemented router for web app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import LandingPage from "./components/LandingPage";
+import ImportNew from "./components/ImportNew";
+import ShowResults from "./components/ShowResults";
 
 function App() {
-
+    return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/import-new" element={< ImportNew />} />
+          <Route path="/show-results" element={< ShowResults />} />
+        </Routes>
+      </div>
+    </Router>
+    )
 }
 
 export default App;
